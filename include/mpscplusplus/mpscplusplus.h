1758ca3b9fea6e96b90bfc7b2c30902901973cbd
James Triantafylos <JTriantafylos@gmail.com>
2021-05-26 01:42:06 -0400

    Implement doxygen doc generation

--- include/mpscplusplus/mpscplusplus.h
+++ include/mpscplusplus/mpscplusplus.h
@@ -23,7 +23,15 @@
 #include <mutex>
 #include <queue>
 
+/**
+ * The namespace encapsulating all mpscplusplus functionality.
+ */
 namespace mpscplusplus {
+    /**
+     * A wrapper structure around std::queue to allow for thread-safe operations.
+     * Uses a @c std::mutex and a @c std::condition_variable to accomplish this.
+     * @tparam T The type of object the queue will be storing.
+     */
     template <typename T>
     class Queue {
       private:
@@ -32,6 +40,10 @@ namespace mpscplusplus {
         std::condition_variable m_condition_variable;
 
       public:
+        /**
+         * Pushes the given object to the back of the queue.
+         * @param[in] data The const lvalue reference to be pushed to the queue.
+         */
         void push(const T& data) {
             std::unique_lock<std::mutex> lock(m_mutex);
             m_backing_queue.push(data);
@@ -39,6 +51,10 @@ namespace mpscplusplus {
             m_condition_variable.notify_one();
         };
 
+        /**
+         * Pushes the given object to the back of the queue.
+         * @param[in] data The rvalue reference to be pushed to the queue.
+         */
         void push(T&& data) {
             std::unique_lock<std::mutex> lock(m_mutex);
             m_backing_queue.push(data);
@@ -46,6 +62,12 @@ namespace mpscplusplus {
             m_condition_variable.notify_one();
         };
 
+        /**
+         * Pops an object from the front of the queue without blocking. This function will return immediately if the
+         * queue is empty.
+         * @param[out] data A reference to where the popped object will be stored.
+         * @return true if an object was popped from the front of the queue, otherwise false.
+         */
         bool pop(T& data) {
             std::unique_lock<std::mutex> lock(m_mutex);
             if (m_backing_queue.empty()) {
@@ -57,6 +79,11 @@ namespace mpscplusplus {
             }
         };
 
+        /**
+         * Pops an object from the front of the queue. This function will wait indefinitely for an object to be pushed
+         * to the queue if the queue is empty.
+         * @param[out] data A reference to where the popped object will be stored.
+         */
         void wait_and_pop(T& data) {
             std::unique_lock<std::mutex> lock(m_mutex);
             if (m_backing_queue.empty()) {
@@ -66,6 +93,12 @@ namespace mpscplusplus {
             m_backing_queue.pop();
         };
 
+        /**
+         * Pops an object from the front of the queue. This function will wait for as long as the specified timeout for
+         * an object to be pushed to the queue if the queue is empty.
+         * @param[out] data A reference to where the popped object will be stored.
+         * @param[in] timeout A reference to a duration of how long this function should wait before returning.
+         */
         template <typename Rep, typename Period>
         void wait_and_pop(T& data, const std::chrono::duration<Rep, Period>& timeout) {
             std::unique_lock<std::mutex> lock(m_mutex);
